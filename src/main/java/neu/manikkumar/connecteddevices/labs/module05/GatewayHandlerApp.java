6f7cef01b710f94b48720c30bec3206194f97995
Manik <mnk_400@icloud.com>
2020-02-24 03:02:52 -0500

    Merge after this commit.

--- src/main/java/neu/manikkumar/connecteddevices/labs/module05/GatewayHandlerApp.java
+++ src/main/java/neu/manikkumar/connecteddevices/labs/module05/GatewayHandlerApp.java
@@ -7,6 +7,10 @@ import neu.manikkumar.connecteddevices.labs.module05.GatewayDataManager;
  */
 public class GatewayHandlerApp {
     public static void main(String[] args) throws Exception {
+        /*
+        Module05 main funciton
+        Set enable = True to enable listener
+        */
         GatewayDataManager dataManager = new GatewayDataManager();
         GatewayDataManager.enable = true;
         dataManager.run(); 
